f7536b70fee04c72704a1ac4c9056cd82737b1b2
Eunji1217 <unji1361@gmail.com>
2024-03-25 17:22:20 +0900

    project setting

---
+++ src/main/java/team/ftft/project4242/Project4242Application.java
@@ -0,0 +1,13 @@
+package team.ftft.project4242;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project4242Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project4242Application.class, args);
+    }
+
+}
---
+++ src/test/java/team/ftft/project4242/Project4242ApplicationTests.java
@@ -0,0 +1,13 @@
+package team.ftft.project4242;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project4242ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
