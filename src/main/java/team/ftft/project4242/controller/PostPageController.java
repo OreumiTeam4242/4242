059b3a5b43e4bc3f9559eddacad6bc8d0b6802d0
LeeJuWon <woen1016@gmail.com>
2024-04-04 22:07:59 +0900

    [Feat : 작성, 조회 멤버 기준으로 UI 변경 중간 push]

--- src/main/java/team/ftft/project4242/controller/PostPageController.java
+++ src/main/java/team/ftft/project4242/controller/PostPageController.java
@@ -5,10 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import team.ftft.project4242.domain.Post;
-import team.ftft.project4242.dto.MemberResponseDto;
 import team.ftft.project4242.dto.PostResponseDto;
 import team.ftft.project4242.service.PostService;
 
+import java.time.LocalDate;
 import java.util.List;
 
 
@@ -32,6 +32,7 @@ public class PostPageController {
     public String postDetail(@PathVariable Long id, Model model){
         Post post = postService.findById(id);
         model.addAttribute("post", new PostResponseDto(post));
+        model.addAttribute("now", LocalDate.now());
 
         return "recruitPostDetail";
     }
