edc6407dedde16ca59f8fee818048c50f72b4ea3
Eunji1217 <unji1361@gmail.com>
2024-04-07 02:18:37 +0900

    [Feat : 신청글 목록 조회]

--- src/main/java/team/ftft/project4242/repository/ApplyRepository.java
+++ src/main/java/team/ftft/project4242/repository/ApplyRepository.java
@@ -1,9 +1,15 @@
 package team.ftft.project4242.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import team.ftft.project4242.domain.Apply;
 
+
 @Repository
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
+    @Query("SELECT a FROM Apply a WHERE a.post.post_id = :postId")
+    Page<Apply> findAllByPostId(Long postId, Pageable pageable);
 }
