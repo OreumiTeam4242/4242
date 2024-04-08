acca20833b5bdf51e1246c3e9d9f003e4a2c9944
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2024-04-08 11:17:33 +0900

    [Feat : 탈퇴 연동]

--- src/main/resources/static/js/personal_edit_page.js
+++ src/main/resources/static/js/personal_edit_page.js
@@ -1,3 +1,57 @@
+const modal = document.querySelector('.modal');
+const modalCloseButton = document.querySelector('#modal_close');
+const modalConfirmButton = document.querySelector('#modal_confirm');
+const submitButton = document.getElementById('delete-user');
+
+// 제출 버튼 클릭 이벤트
+submitButton.addEventListener('click', function () {
+    modal.style.display = 'block';
+});
+
+// 모달 닫기 버튼 클릭 이벤트
+modalCloseButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+// 모달 확인 버튼 클릭 이벤트
+modalConfirmButton.addEventListener('click', function() {
+    // 여기에 제출 로직을 작성할 수 있습니다. 예를 들면, 폼을 서버로 전송하는 코드를 추가할 수 있습니다.
+    // formSubmit();
+    modal.style.display = 'none';
+});
+window.addEventListener('click', function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+//회원탈퇴
+$(document).ready(function() {
+    $('#modal_confirm').click(function() {
+        $.ajax({
+            url: '/api/members/delete', // 요청을 보낼 URL
+            type: 'DELETE', // HTTP 메소드
+            beforeSend: function(xhr) {
+                // 요청을 보내기 전에 헤더에 CSRF 토큰을 설정합니다.
+                // Spring Security를 사용하는 경우 CSRF 보호가 활성화되어 있을 수 있으므로,
+                // 적절한 토큰 값을 설정해야 합니다.
+                // xhr.setRequestHeader('X-CSRF-TOKEN', 'YOUR_CSRF_TOKEN_HERE');
+            },
+            success: function(response) {
+                // 요청이 성공했을 때 실행될 콜백 함수
+                // 예를 들어, 성공 메시지를 표시하거나, 사용자를 다른 페이지로 리디렉션할 수 있습니다.
+                alert('회원 탈퇴가 성공적으로 처리되었습니다.');
+                window.location.href = '/page/intro'; // 홈페이지나 로그인 페이지로 리디렉션
+            },
+            error: function(xhr, status, error) {
+                // 요청이 실패했을 때 실행될 콜백 함수
+                // 예를 들어, 에러 메시지를 표시합니다.
+                alert('오류가 발생했습니다. 다시 시도해주세요.');
+            }
+        });
+
+    });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
 
     document.querySelector('.login_top').addEventListener('click', function () {
